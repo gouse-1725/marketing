2df90741116b15f06ef083f23d6daa406d817d27
gouse-1725 <abdulgouse757@gmail.com>
2025-08-05 10:59:41 +0530

    Added initial project files

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 python manage.py migrate
 python manage.py createsuperuser_auto
